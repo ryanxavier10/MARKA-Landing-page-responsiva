137d7a9f10dca0acc0f761bba47d1ed94a2a287a
Ryan Xavier <lugialga@gmail.com>
2024-03-31 18:49:29 -0300

    projeto final

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function menuShow(){
         document.querySelector('.icon').src = "img/close.svg";
     } 
 }
+function escrevendoLetra(){
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 75 * i)
+        });
+    }
+    
+    const título = document.querySelector('.digitando');
+    ativaLetra(título);
+}
